aa68b7c2233483609acc60b24b1e5f3c1a5f7f36
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-01 23:24:34 +0100

    investigating appveyor fail

--- test/main.cpp
+++ test/main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     int stat = RUN_ALL_TESTS();
+    C4_LOGP("tests finished with status {}", stat);
     return stat;
 }
