2f4da4ae869e05a4bb65bce435ae2c46b53ce7cc
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-02 12:40:34 +0100

    appveyor WTF??

--- test/c4/test/list.split_fwd.fixed.cpp
+++ test/c4/test/list.split_fwd.fixed.cpp
@@ -3,7 +3,7 @@
 C4_BEGIN_NAMESPACE(c4)
 
 #define _c4adaptor(tyname, ty) \
-    _C4_CALL_SPLIT_FWD_LIST_TESTS_FOR_STORAGE(tyname, ty, raw_fixed, _C4_RAW_FIXED, 256)
+    _C4_CALL_SPLIT_FWD_LIST_TESTS_FOR_STORAGE(tyname, ty, raw_fixed, _C4_RAW_FIXED, 32)
 
 CALL_FOR_CONTAINEE_ARCHETYPES(_c4adaptor)
 
