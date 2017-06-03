8eb0971c7515a0f54f54eb4b8293cfd9d3e46653
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-03 23:11:05 +0100

    travis

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -3,27 +3,37 @@
 set -e
 set -x
 
-pwd
 
-dpkg -L clang-3.7 || echo "none"
+if [ $COMPILER == "clang++-3-7" ] ; then
+    cat >> /etc/apt/sources.list <<EOF
 
-export C_COMPILER=$(echo "$COMPILER" | sed 's:clang++:clang:g' | sed 's:g++:gcc:g')
-$COMPILER --version
-$C_COMPILER --version
-cmake --version
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main
+EOF
+    sudo -E apt-get update
+    sudo -E apt-get install -y clang-3.7
+fi
 
-sudo apt-get install valgrind
+sudo -E apt-get install -y valgrind
 if [ "${BUILD_TYPE}" == "Coverage" -a "${TRAVIS_OS_NAME}" == "linux" ]; then
     PATH=~/.local/bin:${PATH};
     pip install --user --upgrade pip;
     pip install --user cpp-coveralls;
 fi
 
+export C_COMPILER=$(echo "$COMPILER" | sed 's:clang++:clang:g' | sed 's:g++:gcc:g')
+$COMPILER --version
+$C_COMPILER --version
+cmake --version
+
+C4STL_DIR=$(cd .. ; pwd)
+echo $C4STL_DIR
+pwd
+
 # compile and install external libraries
 mkdir -p build/extern_build && cd build/extern_build
 cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
       -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="${EXTRA_FLAGS}" \
-      -DCMAKE_INSTALL_PREFIX=`pwd`/../extern_install ../../extern
+      -DCMAKE_INSTALL_PREFIX=`pwd`/../extern_install $C4STL_DIR/extern
 cmake --build . --config Release
 cd -
 
--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+export C_COMPILER=$(echo "$COMPILER" | sed 's:clang++:clang:g' | sed 's:g++:gcc:g')
+
+C4STL_DIR=$(cd .. ; pwd)
 pwd
 cd build
 pwd
@@ -13,13 +16,13 @@ if [ "$PEDANTIC" == "ON" ] ; then
           -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON \
           -DC4STL_EXTERN_DIR=`pwd`/extern_install \
           -DC4STL_CTEST_OPTIONS="-V" \
-          ..
+          $C4STL_DIR
 else
     cmake -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$COMPILER \
           -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_CXX_FLAGS="$EXTRA_FLAGS" \
           -DC4STL_EXTERN_DIR=`pwd`/extern_install \
           -DC4STL_CTEST_OPTIONS="-V" \
-          ..
+          $C4STL_DIR
 fi
 make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
 
