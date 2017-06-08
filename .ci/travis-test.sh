1f97572749f46522ea2bb90401f9ed8b6ab4bc1c
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-08 14:29:18 +0100

    travis sanitize WTF

--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -10,6 +10,8 @@ C4STL_DIR=$(pwd)
 
 if [ "$SAN" == "ON" ] ; then
     set CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON"
+else
+    set CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=OFF"
 fi
 
 cd build
@@ -17,8 +19,7 @@ cd build
 cmake -DCMAKE_C_COMPILER=$CC_ -DCMAKE_CXX_COMPILER=$CXX_ \
       -DCMAKE_BUILD_TYPE=$BT -DCMAKE_CXX_FLAGS="$XFLAGS" \
       -DC4STL_EXTERN_DIR=`pwd`/extern_install \
-      -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON \
-      $CMFLAGS \
+      -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON $CMFLAGS \
       $C4STL_DIR
 
 make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
