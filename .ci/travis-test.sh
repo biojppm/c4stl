e07fcb700d84d7854c7aa159c4cffefc5337a637
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-08 18:09:42 +0100

    working around travis time limit for individual builds:
    * selectively disable valgrind
    * selectively enable sanitizers

--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -3,16 +3,31 @@
 set -e
 set -x
 
-export CC_=$(echo "$CXX_" | sed 's:clang++:clang:g' | sed 's:g++:gcc:g')
-
 pwd
 C4STL_DIR=$(pwd)
 
-if [ "$SAN" == "ON" ] ; then
-    CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON"
+export CC_=$(echo "$CXX_" | sed 's:clang++:clang:g' | sed 's:g++:gcc:g')
+
+  if [ "$SAN" == "ALL"   ] ; then CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON -DC4STL_SANITIZE_ONLY=ON"
+elif [ "$SAN" == "ASAN"  ] ; then CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON -DC4STL_SANITIZE_ONLY=ON -DC4STL_ASAN=ON  -DC4STL_TSAN=OFF -DC4STL_MSAN=OFF -DC4STL_UBSAN=OFF"
+elif [ "$SAN" == "TSAN"  ] ; then CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON -DC4STL_SANITIZE_ONLY=ON -DC4STL_ASAN=OFF -DC4STL_TSAN=ON  -DC4STL_MSAN=OFF -DC4STL_UBSAN=OFF"
+elif [ "$SAN" == "MSAN"  ] ; then CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON -DC4STL_SANITIZE_ONLY=ON -DC4STL_ASAN=OFF -DC4STL_TSAN=OFF -DC4STL_MSAN=ON  -DC4STL_UBSAN=OFF"
+elif [ "$SAN" == "UBSAN" ] ; then CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=ON -DC4STL_SANITIZE_ONLY=ON -DC4STL_ASAN=OFF -DC4STL_TSAN=OFF -DC4STL_MSAN=OFF -DC4STL_UBSAN=ON"
 else
     CMFLAGS="$CMFLAGS -DC4STL_SANITIZE=OFF"
 fi
+
+if [ "$VG" == "ON" ] ; then
+    CMFLAGS="$CMFLAGS -DC4STL_TEST_VALGRIND=ON"
+elif [ "$VG" == "OFF" ] ; then
+    CMFLAGS="$CMFLAGS -DC4STL_TEST_VALGRIND=OFF"
+fi
+
+if [ ! -z "$CMFLAGS" ] ; then
+    echo "additional cmake flags: $CMFLAGS"
+fi
+
+#--------------------------------------------------
 
 cd build
 
@@ -21,6 +36,8 @@ cmake -DCMAKE_C_COMPILER=$CC_ -DCMAKE_CXX_COMPILER=$CXX_ \
       -DC4STL_EXTERN_DIR=`pwd`/extern_install \
       -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON $CMFLAGS \
       $C4STL_DIR
+
+make help
 
 make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
 cd -
