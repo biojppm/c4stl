ee7a263bb3cac55556f90f2f7388b04c7cbc1178
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-09 16:20:40 +0100

    added PVS Studio preamble needed for free license.

--- src/c4/storage/growth.cpp
+++ src/c4/storage/growth.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "c4/storage/growth.hpp"
 
 C4_BEGIN_NAMESPACE(c4)
