df5acbd203ca15479334179dd26151d9ae65533a
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-12 21:29:36 +0100

    benchmark: list: push_back ~7x std::list

--- bm/list/push_back.cpp
+++ bm/list/push_back.cpp
@@ -17,8 +17,6 @@ struct do_reserve
     static void reserve(L &li, size_t cap)
     {
         li.reserve(cap);
-        //C4_LOGP("reserved {} ----> got {}\n", cap, li.capacity());
-        C4_CHECK_MSG(li.capacity() >= cap, "capacity=%lu range=%lu", (size_t)li.capacity(), (size_t)cap);
     }
 };
 
@@ -35,19 +33,23 @@ template< class List >
 void BM_ListPushBack(bm::State& st)
 {
     List li;
+    using T = typename List::value_type;
     do_reserve<List>::reserve(li, st.range(0));
-    typename List::value_type v{};
+    T v{};
+    size_t count = 0;
     while(st.KeepRunning())
     {
         for(int i = 0, e = st.range(0); i < e; ++i)
         {
             if(li.size() == li.max_size()) li.clear();
             li.push_back(v);
+            ++count;
         }
         li.clear();
     }
-    //C4_LOGP("pushed back {} items\n", count);
     st.SetComplexityN(st.range(0));
+    st.SetItemsProcessed(count);
+    st.SetBytesProcessed(count * sizeof(T));
 }
 
 
@@ -71,28 +73,48 @@ template< class T, class I > using split_list__raw_paged_rt     = c4::split_list
 template< class T, class I > using flat_fwd_list__raw_paged_rt  = c4::flat_fwd_list < T, I, c4::stg::raw_paged_rt< c4::flat_fwd_list_elm<T, I>, I > >;
 template< class T, class I > using split_fwd_list__raw_paged_rt = c4::split_fwd_list< T, I, c4::stg::raw_paged_rt< T, I > >;
 
-#define CALL_BM_FOR_LIST_TYPES(mcr)             \
-    mcr(std::list<int>);                        \
-                                                               \
-    mcr(flat_list__raw<int C4_COMMA uint64_t >);               \
-    mcr(flat_list__raw<int C4_COMMA  int64_t >);               \
-    mcr(flat_list__raw<int C4_COMMA uint32_t >);               \
-    mcr(flat_list__raw<int C4_COMMA  int32_t >);               \
-                                                               \
-    mcr(split_list__raw<int C4_COMMA uint64_t >);               \
-    mcr(split_list__raw<int C4_COMMA  int64_t >);               \
-    mcr(split_list__raw<int C4_COMMA uint32_t >);               \
-    mcr(split_list__raw<int C4_COMMA  int32_t >);               \
-                                                               \
-    mcr(flat_fwd_list__raw<int C4_COMMA uint64_t >);               \
-    mcr(flat_fwd_list__raw<int C4_COMMA  int64_t >);               \
-    mcr(flat_fwd_list__raw<int C4_COMMA uint32_t >);               \
-    mcr(flat_fwd_list__raw<int C4_COMMA  int32_t >);               \
-                                                               \
-    mcr(split_fwd_list__raw<int C4_COMMA uint64_t >);               \
-    mcr(split_fwd_list__raw<int C4_COMMA  int64_t >);               \
-    mcr(split_fwd_list__raw<int C4_COMMA uint32_t >);               \
-    mcr(split_fwd_list__raw<int C4_COMMA  int32_t >);               \
+#define CALL_BM_FOR_LIST_TYPES(mcr, ...)                                \
+    mcr(std::list<int>);                                                \
+                                                                        \
+    mcr(flat_list__raw<int C4_COMMA uint64_t >);                        \
+    mcr(flat_list__raw<int C4_COMMA  int64_t >);                        \
+    mcr(flat_list__raw<int C4_COMMA uint32_t >);                        \
+    mcr(flat_list__raw<int C4_COMMA  int32_t >);                        \
+                                                                        \
+    mcr(split_list__raw<int C4_COMMA uint64_t >);                       \
+    mcr(split_list__raw<int C4_COMMA  int64_t >);                       \
+    mcr(split_list__raw<int C4_COMMA uint32_t >);                       \
+    mcr(split_list__raw<int C4_COMMA  int32_t >);                       \
+                                                                        \
+    mcr(flat_fwd_list__raw<int C4_COMMA uint64_t >);                    \
+    mcr(flat_fwd_list__raw<int C4_COMMA  int64_t >);                    \
+    mcr(flat_fwd_list__raw<int C4_COMMA uint32_t >);                    \
+    mcr(flat_fwd_list__raw<int C4_COMMA  int32_t >);                    \
+                                                                        \
+    mcr(split_fwd_list__raw<int C4_COMMA uint64_t >);                   \
+    mcr(split_fwd_list__raw<int C4_COMMA  int64_t >);                   \
+    mcr(split_fwd_list__raw<int C4_COMMA uint32_t >);                   \
+    mcr(split_fwd_list__raw<int C4_COMMA  int32_t >);                   \
+                                                                        \
+    mcr(flat_list__raw_paged<int C4_COMMA uint64_t C4_COMMA 256 >);     \
+    mcr(flat_list__raw_paged<int C4_COMMA  int64_t C4_COMMA 256 >);     \
+    mcr(flat_list__raw_paged<int C4_COMMA uint32_t C4_COMMA 256 >);     \
+    mcr(flat_list__raw_paged<int C4_COMMA  int32_t C4_COMMA 256 >);     \
+                                                                        \
+    mcr(split_list__raw_paged<int C4_COMMA uint64_t C4_COMMA 256 >);    \
+    mcr(split_list__raw_paged<int C4_COMMA  int64_t C4_COMMA 256 >);    \
+    mcr(split_list__raw_paged<int C4_COMMA uint32_t C4_COMMA 256 >);    \
+    mcr(split_list__raw_paged<int C4_COMMA  int32_t C4_COMMA 256 >);    \
+                                                                        \
+    mcr(flat_fwd_list__raw_paged<int C4_COMMA uint64_t C4_COMMA 256 >); \
+    mcr(flat_fwd_list__raw_paged<int C4_COMMA  int64_t C4_COMMA 256 >); \
+    mcr(flat_fwd_list__raw_paged<int C4_COMMA uint32_t C4_COMMA 256 >); \
+    mcr(flat_fwd_list__raw_paged<int C4_COMMA  int32_t C4_COMMA 256 >); \
+                                                                        \
+    mcr(split_fwd_list__raw_paged<int C4_COMMA uint64_t C4_COMMA 256 >); \
+    mcr(split_fwd_list__raw_paged<int C4_COMMA  int64_t C4_COMMA 256 >); \
+    mcr(split_fwd_list__raw_paged<int C4_COMMA uint32_t C4_COMMA 256 >); \
+    mcr(split_fwd_list__raw_paged<int C4_COMMA  int32_t C4_COMMA 256 >);
 
 #define BM(ty) BENCHMARK_TEMPLATE(BM_ListPushBack, ty)->Range(1<<0, 1<<18)->Complexity()
 CALL_BM_FOR_LIST_TYPES(BM)
