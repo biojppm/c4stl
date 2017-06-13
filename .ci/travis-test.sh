6c5f67bf607e3687478f99070c066267a4e31146
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-13 21:33:59 +0100

    travis sonarqube

--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -33,17 +33,20 @@ fi
 
 #--------------------------------------------------
 
-cd build
+if [ "$SONARQUBE" != "1" ] ; then
+    cd build
+    cmake -DCMAKE_C_COMPILER=$CC_ -DCMAKE_CXX_COMPILER=$CXX_ \
+          -DCMAKE_BUILD_TYPE=$BT -DCMAKE_CXX_FLAGS="$XFLAGS" \
+          -DC4STL_EXTERN_DIR=`pwd`/extern_install \
+          -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON $CMFLAGS \
+          $C4STL_DIR
+    make help | sed 1d | sort
+    make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
+    cd -
+else
+    sonar-scanner
+fi
 
-cmake -DCMAKE_C_COMPILER=$CC_ -DCMAKE_CXX_COMPILER=$CXX_ \
-      -DCMAKE_BUILD_TYPE=$BT -DCMAKE_CXX_FLAGS="$XFLAGS" \
-      -DC4STL_EXTERN_DIR=`pwd`/extern_install \
-      -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON $CMFLAGS \
-      $C4STL_DIR
-
-make help | sed 1d | sort
-
-make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
-cd -
+#--------------------------------------------------
 
 exit 0
