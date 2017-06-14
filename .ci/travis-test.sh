9caaf779ad60cac708b1b114af77106de992357a
Joao Paulo Magalhaes <dev@jpmag.me>
2017-06-14 23:07:25 +0100

    update appveyor and travis

--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -38,7 +38,7 @@ if [ "$SONARQUBE" != "1" ] ; then
     cmake -DCMAKE_C_COMPILER=$CC_ -DCMAKE_CXX_COMPILER=$CXX_ \
           -DCMAKE_BUILD_TYPE=$BT -DCMAKE_CXX_FLAGS="$XFLAGS" \
           -DC4STL_EXTERN_DIR=`pwd`/extern_install \
-          -DC4STL_PEDANTIC=ON -DC4STL_WERROR=ON $CMFLAGS \
+          -DC4STL_DEV=ON $CMFLAGS \
           $C4STL_DIR
     make help | sed 1d | sort
     make CTEST_OUTPUT_ON_FAILURE=1 c4stl-test
